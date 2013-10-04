9bc6554bd4c245c065c08b42010fc5dc3a880051
naiche <naicheab@gmail.com>
2013-10-03 21:17:04 -0300

    Funções de lista testadas

--- listas.h
+++ listas.h
@@ -3,28 +3,35 @@
 *
 * NÃO MODIFIQUE ESTE ARQUIVO.
 */
-#include "uthread.h"
+#include "typedefs.h"
 
 typedef struct TipoTCBList ItemList;
 
-struct TipoTCBList 
+struct TipoTCBList
 {
 	TCB* thTCB;
 	ItemList* proximo;
 };
 
 /*Create a new list
-	return: pointer to the first element of the list 
+	return: pointer to the first element of the list
 */
 ItemList* createList(void);
-//
+
+
+/*Print tid of all elements from the list on the screen
+	ItemList *threadList: pointer to the first element of the list
+	return: size of the list if successful
+*/
+int showList(ItemList *threadList);
+
 
 /*Inserts an element at the end of the list
 	ItemList *threadList: pointer to the first element of the list
-	TCB *aThread: TCB of the element to be inserted	
+	TCB *aThread: TCB of the element to be inserted
 	return: size of the list if successful, 0 if not
 */
-int insertList(ItemList *threadList, TCB *aThread);
+int insertList(ItemList **threadList, TCB *aThread);
 
 
 /*Remove an element from the list
@@ -32,18 +39,18 @@ int insertList(ItemList *threadList, TCB *aThread);
 	int tid = id of the item to be removed
 	return: a pointer to the removed item
 */
-TCB* removeFromList(ItemList *threadList, int tid);
+TCB* removeFromList(ItemList **threadList, int tid);
 
 
 /*Remove the first element from the list
 	ItemList *threadList = pointer to the first element of the list
 	return: a pointer to the removed item
 */
-TCB* removeFirstList(ItemList *threadList);
+TCB* removeFirstList(ItemList **threadList);
 
 
 /*Destroy a list
 	ItemList *threadList: pointer to the first element of the list
-	return: Null if successful 
+	return: Null if successful
 */
 ItemList* destroy(ItemList* threadList);
